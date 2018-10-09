1ab82c7e507d2ce93bde760f9fc383eaa560caa9
BP-norn <ryo502045@gmail.com>
2018-10-09 16:21:44 +0900

    python

---
+++ Content/Scripts/HelloWorld.py
@@ -0,0 +1,4 @@
+import unreal_engine as ue
+
+text = "Hello,World"
+ue.print_string(text)
--- Content/Scripts/wiki4.py
+++ Content/Scripts/wiki4.py
@@ -1,9 +1,11 @@
-﻿from gensim.models import word2vec
-model = word2vec.Word2Vec.load("./espanol5.model")
+﻿import unreal_engine as ue
+from gensim.models import word2vec
+
+model = word2vec.Word2Vec.load("espanol5.model")
 print("好きなスペイン語の単語を入力してください。コサイン類似度が高い順に表示します。")
 xxx = input()
 print(xxx,"が入力されました。")
 ret = model.wv.most_similar(positive = xxx ) 
 
 for item in ret:
-    print(item[0], item[1])
+    ue.print_string(item[0], item[1])
