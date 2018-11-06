aa3f5a0d8f6fb7d1e26058df17e734a41ce2149d
BP-norn <ryo502045@gmail.com>
2018-11-07 01:15:02 +0900

    python

---
+++ Source/Emotion_Project_C/APython_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "APython_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMOTION_PROJECT_C_API AAPython_HUD : public AHUD
+{
+	GENERATED_BODY()
+
+};
